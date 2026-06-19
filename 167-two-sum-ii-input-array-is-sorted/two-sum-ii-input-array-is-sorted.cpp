03e0395df7294c632425892b15d4ff1c20ad68b1
vinaykushwaha853-ops <vinaykushwaha853@gmail.com>
2026-06-19 23:03:05 +0530

    Update two-sum-ii-input-array-is-sorted.cpp

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -18,3 +18,27 @@ public:
         return {};
     }
 };
+________________
+Second 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return {i + 1, j + 1}; // 1-based indexing
+                }
+            }
+        }
+
+        return {};
+    }
+};
+
+
+
+
+
+
