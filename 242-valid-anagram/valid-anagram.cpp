476a557c7a7d015f1ec69c4dfde99e3b1f5b883d
vinaykushwaha853-ops <vinaykushwaha853@gmail.com>
2026-07-29 13:46:29 +0530

    Implement isAnagram using frequency map
    Refactor isAnagram function to use unordered_map for frequency counting.

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -19,3 +19,35 @@ public:
 
     }
 };
+Agar question me sirf lowercase English letters hain → int freq[26] ⭐ (Best)
+Agar uppercase, lowercase, Unicode ya generic characters ho sakte hain → unordered_map<char,int> ⭐
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+
+        unordered_map<char, int> frequency;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            frequency[s[i]]++;
+            frequency[t[i]]--;
+        }
+
+        for (auto item : frequency)
+        {
+            if (item.second != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
