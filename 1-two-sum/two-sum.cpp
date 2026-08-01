1474b70149f399c4ff006f73fcc295f201976f3d
vinaykushwaha853-ops <vinaykushwaha853@gmail.com>
2026-08-01 18:23:50 +0530

    Add hashmap method for two-sum solution
    Implemented a second method for the two-sum problem using a hashmap for improved efficiency.

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -13,3 +13,22 @@ public:
     }
   
 };
+
+method2 by hashmap
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> mp;
+    for(int i=0; i<nums.size();i++){
+        int required=target-nums[i];
+        if(mp.count(required)){
+            return {mp[required],i};
+        }
+
+        mp[nums[i]]=i;
+
+    }
+    return { };
+    }
+  
+};
