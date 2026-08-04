b6f89719a4f04f7b21b4e71d61c330bfd66d3609
vinaykushwaha853-ops <vinaykushwaha853@gmail.com>
2026-08-04 07:23:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (90.42%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+
+        int index = word.find(ch);
+
+        if (index == string::npos)
+            return word;
+
+        reverse(word.begin(), word.begin() + index + 1);
+
+        return word;
+    }
+};
