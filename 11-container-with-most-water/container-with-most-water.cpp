d2d68e8439446286f4b730248861690776ccdf38
vinaykushwaha853-ops <vinaykushwaha853@gmail.com>
2026-08-13 23:55:50 +0530

    Time: 3 ms (34.38%) | Memory: 62.9 MB (49.70%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -7,7 +7,7 @@ public:
         while(i<j){
            int area=min(height[i],height[j])*(j-i);
             ans=max(area,ans);
-            if(height[i]<=height[j]){
+            if(height[i]<height[j]){
                 i++;
             }
             else
