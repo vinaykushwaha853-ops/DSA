5208f2781d309cc3296c5dc2fb3b83c96ff899af
vinaykushwaha853-ops <vinaykushwaha853@gmail.com>
2026-09-14 00:16:43 +0530

    Time: 2 ms (3.34%) | Memory: 8.9 MB (33.22%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -2,28 +2,14 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
 
-        int i = s.size() - 1;
-        int len = 0;
-
-        while (i >= 0 && s[i] == ' ')
-            i--;
-
-        while (i >= 0 && s[i] != ' ') {
-            len++;
-            i--;
+        int count =0;
+     for(int i=s.size()-1;i>=0;i--){
+      if(s[i]==' ' && count>0){
+        break;
         }
-
-        return len;
+       if(s[i]!= ' '){
+        count++;}
+        }
+        return count;
     }
 };
-
-// 2 method to
-int count =0
-for(int i=s.size()-1;i>=0;i--){
-    if(s[i]==' ' && count>0){
-        break;
-    }
-    if(s[i]!= ' '){
-        count++;
-    }
-}
